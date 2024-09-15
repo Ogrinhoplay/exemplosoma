7eb9ab8504158445ba7fca795ff32b5c824640db
devpenafort <devpenafort@gmail.com>
2024-09-15 18:29:01 -0300

    feat:Primeira Versao

---
+++ exemplo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int a;
+    int b;
+
+    cout <<    "Exemplo Inicial de Aplicativo" << endl;
+    cout <<    "Digite um numero: ";
+    cin >> a;
+    cout << "Digite outro numero: ";
+    cin >> b; 
+    cout << "A soma dos numeros e: " << a + b << endl;
+
+    system("pause");
+    return 0;
+
+}
